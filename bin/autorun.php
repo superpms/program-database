cf5edd589f17742ebbf133736a863afd751a2afc
Guest <kid666@88.com>
2025-11-03 17:20:16 +0800

    调整pms-basic依赖关系

--- bin/autorun.php
+++ bin/autorun.php
@@ -1,34 +1,38 @@
 <?php
-\pms\hook\LifecycleHook::mount(function () {
-    $dbConfig = config('database');
-    if ($dbConfig !== null) {
-        \think\facade\Db::setConfig($dbConfig);
-    }
-});
+if(class_exists('\pms\hook\LifecycleHook')){
+    \pms\hook\LifecycleHook::mount(LIFECYCLE_BOOT,function () {
+        $dbConfig = config('database');
+        if ($dbConfig !== null) {
+            \think\facade\Db::setConfig($dbConfig);
+        }
+    });
+}
 
 if (in_swoole()) {
-    if (defined(SWOOLE_HTTP_LIFECYCLE_START)) {
-        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_HTTP_LIFECYCLE_START, function () {
+    if(class_exists('\pms\hook\SwooleHttpLifecycleHook')){
+        \pms\hook\SwooleHttpLifecycleHook::mount(LIFECYCLE_BOOT, function () {
             $dbConfig = config('database');
             if ($dbConfig !== null) {
                 foreach ($dbConfig['connections'] ?? [] as $key => $value){
                     $type = $value['type'] ?? 'mysql';
                     switch ($type){
                         case 'mysql':
-                            $dbConfig['connections'][$key]['type'] = PDB_MYSQL_POOL_CONNECTOR;
-                            $dbConfig['connections'][$key]['builder'] = PDB_MYSQL_BUILDER;
+                            $dbConfig['connections'][$key]['type'] = \pms\program\database\connector\MysqlPool::class;
+                            $dbConfig['connections'][$key]['builder'] = \think\db\builder\Mysql::class;
                             break;
                     }
                 }
                 \think\facade\Db::setConfig($dbConfig);
             }
         });
-        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_HTTP_LIFECYCLE_REQUEST_DESTRUCT, function () {
+        \pms\hook\SwooleHttpLifecycleHook::mount(SWOOLE_LIFECYCLE_HTTP_REQUEST_DESTRUCT, function () {
             try {
                 pdb_pool_autoclose();
             } catch (\Throwable $e) {
                 echo "数据库连接错误：" . $e->getMessage() . "\r\n";
             }
         });
+
     }
+
 }
--- bin/const.php
+++ bin/const.php
@@ -1,3 +1,1 @@
 <?php
-const PDB_MYSQL_BUILDER = \think\db\builder\Mysql::class;
-const PDB_MYSQL_POOL_CONNECTOR = \pms\program\database\connector\MysqlPool::class;
