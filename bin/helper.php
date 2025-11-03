b1087a4c995128264a90474fa248d9fc13e6f31a
Guest <kid666@88.com>
2025-11-03 17:31:31 +0800

    调整pms-basic依赖关系

--- bin/helper.php
+++ bin/helper.php
@@ -1,4 +1,10 @@
 <?php
+if (!function_exists('in_swoole')) {
+    function in_swoole(): bool
+    {
+        return defined('SWOOLE_VERSION') && SWOOLE_VERSION !== null;
+    }
+}
 function pdb_pool_autoclose(): void{
     $connector = \think\facade\Db::getInstance();
     if(!empty($connector)){
